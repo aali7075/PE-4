690d53b2d83539ef3465586a154ead5f803c42ce
aaron <aali7075@colorado.edu>
2020-02-12 16:34:04 -0800

    fixed DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,9 +6,19 @@ std::vector< std::vector<std::string> > CreateBoard(){
 }
 
 
+void DisplayBoard(std::vector< std::vector<std::string> > board){
+
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      std::cout <<board[i][j]<< " ";
+    }
+    std::cout << "" << '\n';
+  }
+}
 
 
 
 int main() {
   std::vector< std::vector<std::string> > board = CreateBoard();
+  DisplayBoard(board);
 }
