964b7e1a8f152a488d7219b8a3c27c655d23539c
Vincent-Zag <adza0810@colorado.edu>
2020-02-12 18:21:31 -0800

    Added Comments into TicTacToe.cpp, Added brief summary of the README.md and make cleaned.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Problem Excercise 4 for CSCI 3010
 
 Person 1: Aaron Li
 Person 2: Vincent Zagala
+
+Program takes in user input for row and column, then asking if its a X marker or a O marker.
+
+run the code by make, and ./game
--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,21 +1,21 @@
 #include <vector>
 #include <iostream>
 
-struct Location{
+struct Location{//struct that holds the row and column of the Location
 	int row;
 	int col;
 };
 
 std::vector<std::vector<std::string>> CreateBoard(){
   return {{"[]","[]","[]"},{"[]","[]","[]"},{"[]","[]","[]"}};
-}
+}//sets up the empty board as [] as placeholders
 
 
 
-void DisplayBoard(std::vector< std::vector<std::string> > board){
+void DisplayBoard(std::vector< std::vector<std::string> > board){//displays the board
 
   for(int i=0; i<3; i++){
-    for(int j=0; j<3; j++){
+    for(int j=0; j<3; j++){//sets up the 2d array and prints the board
       std::cout <<board[i][j]<< " ";
     }
     std::cout << "" << '\n';
@@ -28,7 +28,7 @@ Location GetPlayerChoice(){
 	std::cin >> choice.row;
 	std::cout << "Input Column Location Index Starts At 0: ";
 	std::cin >> choice.col;
-	std::cout << "\n";
+	std::cout << "\n";//takes in the player input and returns the struct ahtt holds the row and col of the choice
 	return choice;
 }
 
@@ -36,17 +36,17 @@ void PlaceMarker(std::vector< std::vector<std::string> > &board, Location pos){
   std::string marker;
   std::cout << "Please enter your marker" << '\n';
   std::cin>>marker;
-  board[pos.row][pos.col]=marker;
+  board[pos.row][pos.col]=marker;//places the location gathered from GetPlayerChoice and places the marker the user puts selects in that position
 }
 
 int main() {
 
-  std::vector< std::vector<std::string> > board = CreateBoard();
-	for(int i=0; i<9; i++){
+  std::vector< std::vector<std::string> > board = CreateBoard();// creates an instance of the board
+	for(int i=0; i<9; i++){//9 turns
 	std::cout << "STATE OF THE GAME"<<'\n';
 	std::cout << "Turn " << i<< '\n'<<'\n';
-	DisplayBoard(board);
-  Location result = GetPlayerChoice();
-	PlaceMarker(board, result);
+	DisplayBoard(board);//prints out the turn number and then displays the board
+  Location result = GetPlayerChoice();//calls the function and setting the reulst struct into the returned position
+	PlaceMarker(board, result);//places the set result in to the board.
 	}
 }
