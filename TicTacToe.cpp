3e56e3bafda216672c89cd38b60f4ffcdadb9b99
Vincent-Zag <adza0810@colorado.edu>
2020-02-12 17:32:18 -0800

    Added Get Player Choice

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,20 +1,40 @@
 #include <vector>
 #include <iostream>
 
-std::vector< std::vector<std::string> > CreateBoard(){
-  return {{ "", "", ""}, { "", "", "" }, { "","", "" } };
+struct Location{
+	int row;
+	int col;
+};
+
+std::vector<std::vector<std::string>> CreateBoard(){
+  return {{"[]","[]","[]"},{"[]","[]","[]"},{"[]","[]","[]"}};
 }
 
 void DrawBoard(std::vector<std::vector<std::string>> b){
-for (int i = 0; i < 3 ; i++){
-	for (int j = 0; j < 3; j++){
-	std::cout << b[i][j];
+	for (int i = 0; i < 3 ; i++){
+		for (int j = 0; j < 3; j++){
+		std::cout << b[i][j];
+		}
+		std::cout << "\n";
 	}
-	std::cout << endl;
+}
+
+Location GetPlayerChoice(){
+	Location choice;
+	std::cout << "Input Row Location: ";
+	std::cin >> choice.row;
+	std::cout << "Input Column Location: ";
+	std::cin >> choice.col;
+	std::cout << "\n";
+	return choice;
 }
 
 
-
 int main() {
+
   std::vector< std::vector<std::string> > board = CreateBoard();
+  DrawBoard(board);
+  Location result = GetPlayerChoice();
+  PlaceMarker(result);
+  DrawBoard(board);
 }
