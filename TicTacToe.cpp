94fda9cfeaf7433e87519c218873d937134dfcaf
aaron <aali7075@colorado.edu>
2020-02-12 17:32:12 -0800

    Added place marker

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 std::vector< std::vector<std::string> > CreateBoard(){
-  return {{ "", "", ""}, { "", "", "" }, { "","", "" } };
+  return {{ "NaN", "NaN", "NaN"}, { "NaN", "NaN", "NaN" }, { "NaN","NaN", "NaN" } };
 }
 
 
@@ -17,8 +17,21 @@ void DisplayBoard(std::vector< std::vector<std::string> > board){
 }
 
 
+void PlaceMarker(std::vector< std::vector<std::string> > &board, std::vector<int> pos ){
+  std::string marker;
+  std::cout << "Please enter your marker" << '\n';
+  std::cin>>marker;
+  board[pos[0]][pos[1]]=marker;
+}
 
 int main() {
+
   std::vector< std::vector<std::string> > board = CreateBoard();
   DisplayBoard(board);
+
+  PlaceMarker(board, v);
+
+  std::cout << "STATE OF THE GAME" << '\n'<<'\n';
+  DisplayBoard(board);
+
 }
