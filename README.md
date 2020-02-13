07d9c43fd55a6eca3cf29cb690d852ea57567a62
aaron <aali7075@colorado.edu>
2020-02-12 17:54:43 -0800

    Finished code and created output.txt

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PE-4
 Problem Excercise 4 for CSCI 3010
+
+Person 1: Aaron Li
+Person 2: Vincent Zagala
--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,6 @@
 #include <vector>
 #include <iostream>
 
-Person2
 struct Location{
 	int row;
 	int col;
@@ -11,13 +10,6 @@ std::vector<std::vector<std::string>> CreateBoard(){
   return {{"[]","[]","[]"},{"[]","[]","[]"},{"[]","[]","[]"}};
 }
 
-void DrawBoard(std::vector<std::vector<std::string>> b){
-	for (int i = 0; i < 3 ; i++){
-		for (int j = 0; j < 3; j++){
-		std::cout << b[i][j];
-		}
-		std::cout << "\n";
-	}
 
 
 void DisplayBoard(std::vector< std::vector<std::string> > board){
@@ -32,30 +24,29 @@ void DisplayBoard(std::vector< std::vector<std::string> > board){
 
 Location GetPlayerChoice(){
 	Location choice;
-	std::cout << "Input Row Location: ";
+	std::cout << "Input Row Location Index Starts At 0: ";
 	std::cin >> choice.row;
-	std::cout << "Input Column Location: ";
+	std::cout << "Input Column Location Index Starts At 0: ";
 	std::cin >> choice.col;
 	std::cout << "\n";
 	return choice;
 }
 
-void PlaceMarker(std::vector< std::vector<std::string> > &board, std::vector<int> pos ){
+void PlaceMarker(std::vector< std::vector<std::string> > &board, Location pos){
   std::string marker;
   std::cout << "Please enter your marker" << '\n';
   std::cin>>marker;
-  board[pos[0]][pos[1]]=marker;
+  board[pos.row][pos.col]=marker;
 }
 
 int main() {
 
   std::vector< std::vector<std::string> > board = CreateBoard();
-  DrawBoard(board);
+	for(int i=0; i<9; i++){
+	std::cout << "STATE OF THE GAME"<<'\n';
+	std::cout << "Turn " << i<< '\n'<<'\n';
+	DisplayBoard(board);
   Location result = GetPlayerChoice();
-  DrawBoard(board);
-  DisplayBoard(board);
-
-  PlaceMarker(board, v);
-  std::cout << "STATE OF THE GAME" << '\n'<<'\n';
-  DisplayBoard(board);
+	PlaceMarker(board, result);
+	}
 }
